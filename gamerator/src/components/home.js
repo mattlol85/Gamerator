f90df99fa2713713216989ab01aa2cfb6748a4aa
fernrey <Fernando39@live.com>
2022-01-25 00:15:23 -0500

    changes

--- gamerator/src/components/home.js
+++ gamerator/src/components/home.js
@@ -1,19 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './styles/Home.css'
 
-import "../Styles/home.css"; 
+export default function Home() {
 
- 
-export default function home() {
-  return (
-    <div className="homecontainer">
-      
-      <h1>Popular Trends</h1>
-      <h3>based on voting count!</h3>
+    // Hooks
+    // CONNECT EXPRESS SERVER AND GET LIST OF COLLEGE
+    
+    return (
+        <div className="Home">
+            <div className="appNav">
+                <ul id="navList">
+                    <li >
+                        <Link to="/" id="linkElement" >Home</Link>
+                    </li>
+                    <li >
+                        <Link to="/action" id="linkElement">Action</Link>
+                    </li>
+                    <li >
+                        <Link to="/shooting" id="linkElement">Shooting</Link>
+                    </li>
+                </ul>
+            </div>
+            <h1 id="homeTitle"> Home</h1>
 
-
-        <Link to="/login"> <button className="startbtn"> Start </button> </Link>
-      
-    </div>
-  );
+        </div>
+    )
 }
+
