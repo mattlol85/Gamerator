c4fc0b407c3159d9c75dbdd3f267001d8448c55e
fernrey <Fernando39@live.com>
2022-01-27 11:50:34 -0500

    new styles

--- gamerator/src/components/StartPage.js
+++ gamerator/src/components/StartPage.js
@@ -1,18 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import GoogleLogin from 'react-google-login';
 
+import './styles/StartPage.css';
 export default function StartPage(){
+
+    const responseGoogle = (response) => {
+  console.log(response);
+}
 return(
     <div className="StartPage">
     {/* <div className="Logo"> Welcome to GameRater</div> */}
-    <h1>Welcome to GamerRater!</h1>
-
-    <Link className="home" to="/home">
-        Home
-      </Link >
-    <Link className="login" to="/login">
-        Log in using Google
-      </Link >
+    <h1 className="animate">Welcome to GamerRater!</h1>
+    <h2 className="log"> Log in </h2>
+    <Link to="/home">
+       <button className="HomeLink"> Home </button>
+    </Link > 
+     <GoogleLogin
+    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+    className="googleLogin"
+  />
     </div>
+
 )
 }
