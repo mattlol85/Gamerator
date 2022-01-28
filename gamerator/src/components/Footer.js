e67d39f90e42c80662099e22c9529da27048edf1
LeonardoGalan <leonardogalan2@gmail.com>
2022-01-28 01:11:48 -0500

    changed footer color

--- gamerator/src/components/Footer.js
+++ gamerator/src/components/Footer.js
@@ -8,10 +8,10 @@ export default function Footer() {
     return (
       <div id="footer">
         <div id="footerItems">
-          <ExternalLink id="item" href="https://www.linkedin.com/in/fernrey/" >Fernando Reyes |</ExternalLink>
-          <ExternalLink id="item" href="https://www.linkedin.com/in/matthew-fitzgerald-3040b6160/"> Matthew Fitzgerald |</ExternalLink>
-          <ExternalLink id="item" href="https://www.linkedin.com/in/leonardogalan/"> Leonardo Galan |</ExternalLink>
-          <ExternalLink id="item" href="https://www.linkedin.com/in/ramon-m-polanco/"> Ramon Polanco</ExternalLink>
+          <ExternalLink href="https://www.linkedin.com/in/fernrey/"  style={{textDecoration:"none", color: "#5c8fff"}}>Fernando Reyes |</ExternalLink>
+          <ExternalLink href="https://www.linkedin.com/in/matthew-fitzgerald-3040b6160/"  style={{textDecoration:"none", color: "#5c8fff"}}> Matthew Fitzgerald |</ExternalLink>
+          <ExternalLink href="https://www.linkedin.com/in/leonardogalan/"  style={{textDecoration:"none", color: "#5c8fff"}}> Leonardo Galan |</ExternalLink>
+          <ExternalLink href="https://www.linkedin.com/in/ramon-m-polanco/" style={{textDecoration:"none", color: "#5c8fff"}}> Ramon Polanco |</ExternalLink>
         </div>
         <div id="bottomOfFooter" className="center">
           Ⓒ Group 6
