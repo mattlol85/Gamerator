d27aece4f58b6a6876677c779b06b20a9121a5a0
Matthew Fitzgerald <mattlol85@gmail.com>
2022-01-28 00:23:53 -0500

    footer fix

--- gamerator/src/components/Footer.js
+++ gamerator/src/components/Footer.js
@@ -8,10 +8,10 @@ export default function Footer() {
     return (
       <div id="footer">
         <div id="footerItems">
-          <ExternalLink href="https://www.linkedin.com/in/fernrey/" >Fernando Reyes |</ExternalLink>
-          <ExternalLink href="https://www.linkedin.com/in/matthew-fitzgerald-3040b6160/"> Matthew Fitzgerald |</ExternalLink>
-          <ExternalLink href="https://www.linkedin.com/in/leonardogalan/"> Leonardo Galan |</ExternalLink>
-          <ExternalLink href="https://www.linkedin.com/in/ramon-m-polanco/"> Ramon Polanco |</ExternalLink>
+          <ExternalLink id="item" href="https://www.linkedin.com/in/fernrey/" >Fernando Reyes |</ExternalLink>
+          <ExternalLink id="item" href="https://www.linkedin.com/in/matthew-fitzgerald-3040b6160/"> Matthew Fitzgerald |</ExternalLink>
+          <ExternalLink id="item" href="https://www.linkedin.com/in/leonardogalan/"> Leonardo Galan |</ExternalLink>
+          <ExternalLink id="item" href="https://www.linkedin.com/in/ramon-m-polanco/"> Ramon Polanco</ExternalLink>
         </div>
         <div id="bottomOfFooter" className="center">
           Ⓒ Group 6
