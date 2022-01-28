a15f4321e365d312e7f69e959071faa4f413a11b
rmpolanco <ramonpolanco0205@gmail.com>
2022-01-28 00:13:38 -0500

    Minor styling fixes+image popups

--- gamerator/src/components/Home.js
+++ gamerator/src/components/Home.js
@@ -9,9 +9,9 @@ export default function Home() {
         <div className="homePage">
             <div id="homeContent" >
                 <div className="about">
-                <h2>🎮Welcome To Gamerator</h2>
-                <h3>Gamerator is a place where gamers can come together and see
-                once and for all if the top games really stand the test of the *current* times. Gamerator strides
+                <h2>🎮Welcome To GameRator</h2>
+                <h3>GameRator™ is a place where gamers can come together and see
+                once and for all if the top games really stand the test of the *current* times. GameRator strides
                 to be #1 when it comes to ultra small-scale applications.</h3>
                 </div>
                 <br/>
@@ -27,7 +27,7 @@ export default function Home() {
                 <div className="about">
                 <h2>💙 Made With Love</h2>
                 <p id="smallText">By four handsome programmers...</p>
-                <h3>This application was made using React.JS, Posgres, Node.JS, and Express. The backend of this applicaton is deployed on Heroku
+                <h3>This application was made using React.JS, PostgreSQL, Node.JS, and Express. The backend of this applicaton is deployed on Heroku
                     and the front on Netlify.
                 </h3>
                 </div>
