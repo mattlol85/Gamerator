2b764f59a4faa133afbeeed24702efacad02c148
rmpolanco <82235869+rmpolanco@users.noreply.github.com>
2022-08-14 20:19:28 -0400

    updated hrefs in footer

--- gamerator/src/components/Footer.js
+++ gamerator/src/components/Footer.js
@@ -11,7 +11,7 @@ export default function Footer() {
           <ExternalLink href="https://www.linkedin.com/in/fernrey/"  style={{textDecoration:"none", color: "#5c8fff"}}>Fernando Reyes |</ExternalLink>
           <ExternalLink href="https://www.linkedin.com/in/matthew-fitzgerald-3040b6160/"  style={{textDecoration:"none", color: "#5c8fff"}}> Matthew Fitzgerald |</ExternalLink>
           <ExternalLink href="https://www.linkedin.com/in/leonardogalan/"  style={{textDecoration:"none", color: "#5c8fff"}}> Leonardo Galan |</ExternalLink>
-          <ExternalLink href="https://www.linkedin.com/in/ramon-m-polanco/" style={{textDecoration:"none", color: "#5c8fff"}}> Ramon Polanco |</ExternalLink>
+          <ExternalLink href="https://www.linkedin.com/in/rmpolanco/" style={{textDecoration:"none", color: "#5c8fff"}}> Ramon Polanco |</ExternalLink>
         </div>
         <div id="bottomOfFooter" className="center">
           Ⓒ Group 6
